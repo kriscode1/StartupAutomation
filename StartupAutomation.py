03094f6edba09e8d6398f82bd9b6d3ae17a21613
kriscode1 <kriscode1@users.noreply.github.com>
2017-02-25 12:40:30 -0500

    Add functions for getting windows at coordinates.
    get_parent_window_at_coordinates, get_child_window_at_coordinates, get_window_tree_at_coordinates

--- StartupAutomation.py
+++ StartupAutomation.py
@@ -1,5 +1,7 @@
 '''Functions for common automation and window repositioning tasks.
 
+Author: Kristofer Christakos
+
 Specialized for Windows only.
 '''
 
@@ -8,6 +10,8 @@ import win32gui, win32con, win32process, win32api, win32console
 import time
 
 MULI_KEYPRESS_SLEEP_DELAY = 0.1
+
+#################### Process Related Functions ####################
 
 def run_program(commandLine):
     '''Creates a new process with a new console window.
@@ -22,6 +26,8 @@ def run_program(commandLine):
     #return os.startfile(path)
     return hProcess
 
+#################### Window Related Functions ####################
+
 def get_all_window_handles():
     '''Returns a list of all window handles for all processes.'''
     windowList = []
@@ -31,13 +37,13 @@ def get_all_window_handles():
     success = win32gui.EnumWindows(EnumWindowsProc, None)
     return windowList
 
-def get_process_window_handles(hProcess):
+def get_process_window_handles(process_handle):
     '''Returns a list of all window handles for a single process.
     
-    hProcess is a process handle, which can be saved from run_program()
+    process_handle can be retrieved and saved from run_program()
     '''
     windowList = []
-    pid = win32process.GetProcessId(hProcess)
+    pid = win32process.GetProcessId(process_handle)
     def EnumWindowsProc(hwnd, lParam):
         (threadId, windowPid) = win32process.GetWindowThreadProcessId(hwnd)
         if windowPid == pid:
@@ -46,29 +52,29 @@ def get_process_window_handles(hProcess):
     success = win32gui.EnumWindows(EnumWindowsProc, None)
     return windowList
 
-def get_window_handle(windowName):
-    '''Returns the window handle for the window with the given title.'''
-    return win32gui.FindWindow(None, windowName)
-
 def get_current_window_handle():
     '''Returns the window handle for the console window associated with this 
     process.
     '''
     return win32console.GetConsoleWindow()
 
-def get_window_name(hwnd):
-    '''Returns the title of the window with the given window handle.'''
-    return win32gui.GetWindowText(hwnd)
+def get_window_handle(window_name):
+    '''Returns the window handle for the window with the given title.'''
+    return win32gui.FindWindow(None, window_name)
 
-def get_window_coords(hwnd):
+def get_window_name(window_handle):
+    '''Returns the title of the window with the given window handle.'''
+    return win32gui.GetWindowText(window_handle)
+
+def get_window_coords(window_handle):
     '''Retrieves the coordinates of the window with the given handle.
     
     Coordinates returned in the 4-tuple: (left, top, right, bottom)
     '''
-    (left, top, right, bottom) = win32gui.GetWindowRect(hwnd)
+    (left, top, right, bottom) = win32gui.GetWindowRect(window_handle)
     return (left, top, right, bottom)
 
-def set_window_coords(hwnd, left, top, right=0, bottom=0):
+def set_window_coords(window_handle, left, top, right=0, bottom=0):
     '''Repositions a window to new coordinates.
     
     If the right and bottom coordinates are ignored, the window size is 
@@ -77,52 +83,93 @@ def set_window_coords(hwnd, left, top, right=0, bottom=0):
     width = right - left
     height = bottom - top
     if (right == 0) or (bottom == 0):
-        (curLeft, curTop, curRight, curBottom) = get_window_coords(hwnd)
+        (curLeft, curTop, curRight, curBottom) = get_window_coords(window_handle)
         width = curRight - curLeft
         height = curBottom - curTop
         right = left + width
         bottom = top + height
-    return win32gui.SetWindowPos(hwnd, win32con.HWND_TOP, 
+    return win32gui.SetWindowPos(window_handle, win32con.HWND_TOP, 
                                  left, top, width, height, 0)
 
-def set_window_size(hwnd, width, height):
+def set_window_size(window_handle, width, height):
     '''Resizes a window without repositioning.'''
-    (curLeft, curTop, curRight, curBottom) = get_window_coords(hwnd)
+    (curLeft, curTop, curRight, curBottom) = get_window_coords(window_handle)
     newRight = curLeft + width
     newBottom = curTop + height
-    return setWindowCoords(hwnd, curLeft, curTop, newRight, newBottom)
+    return setWindowCoords(window_handle, curLeft, curTop, newRight, newBottom)
 
-def bring_window_to_foreground(hwnd):
+def bring_window_to_foreground(window_handle):
     '''Sets the foreground window to the window of the given handle.'''
-    win32gui.SetForegroundWindow(hwnd)
+    win32gui.SetForegroundWindow(window_handle)
 
-def press_key(virtualKeyCode):
-    '''Presses the given virtual key down and up with no delay.'''
-    win32api.keybd_event(virtualKeyCode, 0, 0, 0)
-    win32api.keybd_event(virtualKeyCode, 0, 2, 0)
+def get_parent_window_at_coordinates(x, y):
+    '''Returns the handle of the parent window at (x,y) screen coordinates.'''
+    window_handle = win32gui.WindowFromPoint((x,y))
+    if window_handle == 0: window_handle = None
+    return window_handle
 
-def press_shift_something(virtualKeyCode):
-    '''Holds shift while pressing a virtual key, for typing capital letters.'''
-    win32api.keybd_event(win32con.VK_SHIFT, 0, 0, 0)
-    time.sleep(MULI_KEYPRESS_SLEEP_DELAY)
-    press_key(virtualKeyCode)
-    time.sleep(MULI_KEYPRESS_SLEEP_DELAY)
-    win32api.keybd_event(win32con.VK_SHIFT, 0, 2, 0)
+def get_child_window_at_coordinates(x, y):
+    '''Returns the handle of the first child window at (x,y) screen coordinates.
+    
+    Returns None if no child window contains the coordinates.
+    '''
+    parent_handle = win32gui.WindowFromPoint((x,y))
+    if parent_handle == 0: parent_handle = None
+    child_handle = None
+    if (parent_handle != None):
+        child_handle = win32gui.ChildWindowFromPoint(parent_handle, (x,y))
+        if child_handle == 0: child_handle = None
+        if child_handle == parent_handle: child_handle = None
+    return child_handle
+
+def get_window_tree_at_coordinates(x, y):
+    '''Returns a list of window handles at (x,y) screen coordinates.
+    
+    The first handle in the list is the parent window. The other handles 
+    are the first child window of the previous handle that contains the 
+    coordinates. These are iteratively found with 
+    get_child_window_at_coordinates(x, y). 
+    '''
+    window_handles = []
+    parent_handle = get_parent_window_at_coordinates(x, y)
+    while parent_handle != None:
+        window_handles.append(parent_handle)
+        child_handle = win32gui.ChildWindowFromPoint(parent_handle, (x,y))
+        if child_handle == 0: child_handle = None
+        if child_handle == parent_handle: parent_handle = None
+        else: parent_handle = child_handle
+    return window_handles
+
+#################### Keyboard Related Functions ####################
+
+def press_key(virtual_key_code):
+    '''Presses the given virtual key down and up with no delay.
+    
+    Use type_string() instead if possible.
+    '''
+    win32api.keybd_event(virtual_key_code, 0, 0, 0)
+    win32api.keybd_event(virtual_key_code, 0, 2, 0)
 
 def press_return():
-    '''Virtually hits the enter key.'''
+    '''Presses the enter key.'''
     press_key(win32con.VK_RETURN)
 
 def press_printscreen():
     '''Presses the print screen key.'''
     press_key(win32con.VK_SNAPSHOT)
-    time.sleep(MULI_KEYPRESS_SLEEP_DELAY)
 
 def type_string(text):
     '''Virtually types the text string.
     
     Attempts typing capital letters and special character keys.
     '''
+    def press_shift_something(virtual_key_code):
+        '''Holds shift while pressing a virtual key, for typing capital letters.'''
+        win32api.keybd_event(win32con.VK_SHIFT, 0, 0, 0)
+        time.sleep(MULI_KEYPRESS_SLEEP_DELAY)
+        press_key(virtual_key_code)
+        time.sleep(MULI_KEYPRESS_SLEEP_DELAY)
+        win32api.keybd_event(win32con.VK_SHIFT, 0, 2, 0)
     for char in text:
         if 65 <= ord(char) <= 90:
             #Capital letters
@@ -183,11 +230,27 @@ def type_string(text):
             press_shift_something(ord('9'))
         elif char == ')':
             press_shift_something(ord('0'))
+        elif char == '\n':
+            press_return()
         else:
             print("Can't type '"+char+"' key.", ord(char))
             return False
         time.sleep(MULI_KEYPRESS_SLEEP_DELAY)
     return True
+
+def capslock_on():
+    '''Determines if caps lock is pressed, returning True or False.'''
+    capsLockOn = win32api.GetKeyState(win32con.VK_CAPITAL)
+    if capsLockOn:
+        return True
+    else:
+        return False
+
+#################### Cursor Related Functions ####################
+
+def get_cursor():
+    '''Retrieves the cursor screen coordinates (x,y).'''
+    return win32api.GetCursorPos()
 
 def set_cursor(x, y):
     '''Moves the cursor to screen coordinates (x,y).'''
@@ -197,11 +260,3 @@ def left_click():
     '''Simulates a mouse left-click with no delay.'''
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0)
-
-def capslock_on():
-    '''Determines if caps lock is pressed, returning True or False.'''
-    capsLockOn = win32api.GetKeyState(win32con.VK_CAPITAL)
-    if capsLockOn:
-        return True
-    else:
-        return False
